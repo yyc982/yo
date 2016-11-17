d0026cb3af63fbaa729ffeba6dcc5075c5e455de
Yuechen <yuechen@YuechendeMacBook-Pro.local>
2016-11-17 16:06:29 -0500

    add EditText, button and change TextView in activity_main.xml

--- app/src/main/java/com/jiuzhang/guojing/yo/MainActivity.java
+++ app/src/main/java/com/jiuzhang/guojing/yo/MainActivity.java
@@ -2,6 +2,8 @@ package com.jiuzhang.guojing.yo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +14,21 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO 5 get View using findViewById
         // get the EditText, the Button and the TextView using findViewById
+        EditText editText = (EditText) findViewById(R.id.edit_text);
 
         // TODO 6 set OnClickListener of Button
         // set the OnClickListener of the button so that when the button is clicked,
         // your program should set the text of TextView to be the value of the EditText
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                String str = eText.getText().toString();
+                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
+                msg.show();
+            }
+        });
+
+
 
     }
 }
