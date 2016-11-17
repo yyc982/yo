e3f98d36fa90fc43641c262d9db4fbfbc58494d5
Yuechen <yuechen@YuechendeMacBook-Pro.local>
2016-11-17 16:19:28 -0500

    use button OnClickListener to change the value of TextView

--- app/src/main/java/com/jiuzhang/guojing/yo/MainActivity.java
+++ app/src/main/java/com/jiuzhang/guojing/yo/MainActivity.java
@@ -2,8 +2,11 @@ package com.jiuzhang.guojing.yo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,21 +17,31 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO 5 get View using findViewById
         // get the EditText, the Button and the TextView using findViewById
-        EditText editText = (EditText) findViewById(R.id.edit_text);
+        final EditText editText = (EditText) findViewById(R.id.edit_text);
 
         // TODO 6 set OnClickListener of Button
+        final TextView textView = (TextView) findViewById(R.id.text_view);
         // set the OnClickListener of the button so that when the button is clicked,
         // your program should set the text of TextView to be the value of the EditText
         Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new OnClickListener() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                String str = eText.getText().toString();
-                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
-                msg.show();
+                String str = editText.getText().toString();
+                textView.setText(str);
+
             }
         });
 
 
+//        button1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                button1.startAnimation(animation);
+//                Toast.makeText(MainActivity.this, button1.getText() + " " + nameEdit.getText(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+//
 
     }
 }
